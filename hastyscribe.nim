693a70628087fda774bd3dfd3b3b53c5fa4e3fa9
h3rald <h3rald@h3rald.com>
2013-10-06 15:39:39 +0200

    CSS updates.

--- hastyscribe.nim
+++ hastyscribe.nim
@@ -1,4 +1,4 @@
-import os, parseopt, strutils, markdown
+import os, parseopt, strutils, times, markdown
 
 # Source
 const src_css = "assets/hastyscribe.css".slurp
@@ -30,6 +30,15 @@ let source = input_file.readFile
 var metadata = TMDMetaData(title:"", author:"", date:"")
 let body = source.md(MKD_DOTOC or MKD_EXTRA_FOOTNOTE, metadata)
 
+# TODO handle invalid date errors
+
+if metadata.date == "":
+  metadata.date = getDateStr()
+
+var date = metadata.date.split('-')
+
+var timeinfo = TTimeInfo(year: date[0].parseInt, month: TMonth(date[1].parseInt), monthday: date[2].parseInt)
+
 let document = """<!doctype html>
 <html lang="en">
 <head>
@@ -37,20 +46,21 @@ let document = """<!doctype html>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="author" content="$author">
-  <meta name="date" content="$date" scheme="YYYY-MM-DD">
   $css
 </head> 
 <body>
-  <h1>$title</h1>
+  <div id="header">
+    <h1>$title</h1>
+  </div>
   <div id="toc">
     $toc
-  </nav>
-  <div>
-$body
-  </main>
-  <div id="footer">
-    <p>by $author &ndash; Generated with <a href="http://h3rald.com/hastyscribe/">HastyScribe</a></p>
   </div>
-</body>""" % ["title", metadata.title, "author", metadata.author, "date", metadata.date, "toc", metadata.toc, "css", css, "body", body]
+  <div id="main">
+$body
+  </div>
+  <div id="footer">
+    <p>by <em>$author</em> &ndash; Generated with <a href="https://github.com/h3rald/hastyscribe/">HastyScribe</a> on <em>$date</em></p>
+  </div>
+</body>""" % ["title", metadata.title, "author", metadata.author, "date", timeinfo.format("MMMM d, yyyy"), "toc", metadata.toc, "css", css, "body", body]
 
 output_file.writeFile(document)
