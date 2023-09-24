6add7758156ec697df0993195ed825622f1b3295
h3rald <h3rald@h3rald.com>
2023-09-24 13:02:33 +0200

    Incremented version, regenerated guide.

--- src/hastyscribepkg/config.nim
+++ src/hastyscribepkg/config.nim
@@ -1,4 +1,4 @@
 const
-  pkgVersion*     = "2.0.0"
+  pkgVersion*     = "2.1.0"
   pkgAuthor*      = "Fabio Cevasco"
   pkgDescription* = "Self-contained markdown compiler generating self-contained HTML documents"
